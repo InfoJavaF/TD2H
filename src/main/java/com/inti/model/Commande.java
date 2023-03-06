6c75e03475220f123b163f8af81a1fd0ae085780
InfoJavaF <florian.belin@itech.fr>
2023-03-06 14:14:09 +0100

    TD3

--- src/main/java/com/inti/model/Commande.java
+++ src/main/java/com/inti/model/Commande.java
@@ -10,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -21,7 +22,7 @@ public class Commande {
 	
 	private int idCommande;
 	private LocalDate date;
-	
+	@OneToOne
 	@ManyToOne
 	@JoinColumn(name = "idU")
 	private Utilisateur utilisateur;
